8d057047f666aa63445ba0c0d8bad329b1cabd3e
isak0016 <isak999555@icloud.com>
2026-04-28 10:12:29 +0200

    fix

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 cd /Users/isakhaapaniemi/talkies/talkies
 source .venv/bin/activate
 python app.py &
-ngrok http 5000 &
+ngrok http 8080 &
 sleep 2
 NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | python3 -c "import sys,json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])")
 open $NGROK_URL
